1eafed9a31f95816d5d301e35ddcb0444a4ed35e
benny-alfian <ibenkz.hawker@gmail.com>
2026-02-23 04:20:09 +0000

    fix: bind 0.0.0.0 for railway

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🔥 Server running on port ${PORT}`);
 });
 });
