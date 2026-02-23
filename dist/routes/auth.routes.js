bbc287a9afe2e9b70c29c583e7957b0854c1d391
benny-alfian <ibenkz.hawker@gmail.com>
2026-02-23 08:42:10 +0000

    feat: add register route

--- dist/routes/auth.routes.js
+++ dist/routes/auth.routes.js
@@ -3,5 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const auth_controller_1 = require("../controllers/auth.controller");
 const router = (0, express_1.Router)();
+/**
+ * Auth Routes
+ */
+router.post("/register", auth_controller_1.AuthController.register);
 router.post("/login", auth_controller_1.AuthController.login);
 exports.default = router;
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,6 +3,10 @@ import { AuthController } from "../controllers/auth.controller";
 
 const router = Router();
 
+/**
+ * Auth Routes
+ */
+router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
 
 export default router;
